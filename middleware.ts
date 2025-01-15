6421119e6dfe10d50df4b7508cb2d0bee297042a
thenatural86 <philip.smalls86@gmail.com>
2025-01-14 21:03:59 -0500

    adds sign up and sign in modals

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 // public routes in our case '/'
-const isPublicRoute = createRouteMatcher(['/'])
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) auth().protect()
