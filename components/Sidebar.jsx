d81ffc2116127dc137985618d29b91289649b3e8
thenatural86 <philip.smalls86@gmail.com>
2025-01-15 16:59:27 -0500

    adds sidebar components

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,5 +1,18 @@
+import MemberProfile from './MemberProfile'
+import NavLinks from './NavLinks'
+import SidebarHeader from './SidebarHeader'
+
 const Sidebar = () => {
-  return <div className='text-6xl'>Sidebar</div>
+  return (
+    <div className='px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto]'>
+      {/* first row */}
+      <SidebarHeader />
+      {/* second row */}
+      <NavLinks />
+      {/* third row */}
+      <MemberProfile />
+    </div>
+  )
 }
 
 export default Sidebar
