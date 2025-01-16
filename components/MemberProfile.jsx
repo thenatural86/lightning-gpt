d56ae240f1ee5daec2eeb343c6b6c020e0fa24ce
thenatural86 <philip.smalls86@gmail.com>
2025-01-15 20:51:08 -0500

    reverts clerk version

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -1,5 +1,17 @@
-const MemberProfile = () => {
-  return <div>MemberProfile</div>
+import { UserButton } from '@clerk/nextjs'
+// auth and currentUser are now imported from /server
+import { auth, currentUser } from '@clerk/nextjs/server'
+
+const MemberProfile = async () => {
+  const user = await currentUser()
+  const { userId } = auth()
+  console.log(user)
+  return (
+    <div className='px-4 flex items-center gap-2'>
+      <UserButton fallbackRedirectUrl='/' />
+      <p>{user.emailAddresses[0].emailAddress}</p>
+    </div>
+  )
 }
 
 export default MemberProfile
