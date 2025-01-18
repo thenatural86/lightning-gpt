0695fb2c313866f75cd81bbd7a548b8ec232110f
thenatural86 <philip.smalls86@gmail.com>
2025-01-17 23:11:29 -0500

    adds react query

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,12 +1,19 @@
 'use client'
+import { generateChatResponse } from '@/utils/action'
+import { useMutation } from '@tanstack/react-query'
 import { useState } from 'react'
 
 const Chat = () => {
   const [text, setText] = useState('')
   const [messages, setMessages] = useState([])
+
+  const { mutate } = useMutation({
+    mutationFn: (message) => generateChatResponse(message),
+  })
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(text)
+    mutate(text)
   }
   return (
     <div className='min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]'>
