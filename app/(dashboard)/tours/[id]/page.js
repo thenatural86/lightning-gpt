642b7b35c4be15c640c6b089d4b351c0a16af8a7
thenatural86 <philip.smalls86@gmail.com>
2025-01-24 11:53:59 -0500

    adds unsplash images

--- app/(dashboard)/tours/[id]/page.js
+++ app/(dashboard)/tours/[id]/page.js
@@ -3,7 +3,9 @@ import { generateTourImage, getSingleTour } from '@/utils/action'
 import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
+import axios from 'axios'
 import React from 'react'
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=`
 
 const SingleTourPage = async ({ params }) => {
   const tour = await getSingleTour(params.id)
@@ -11,10 +13,13 @@ const SingleTourPage = async ({ params }) => {
     redirect('/tours')
   }
 
-  const tourImage = await generateTourImage({
-    city: tour.city,
-    country: tour.country,
-  })
+  const { data } = await axios(`${url}${tour.city}`)
+  const tourImage = data?.results[0]?.urls?.raw
+
+  // const tourImage = await generateTourImage({
+  //   city: tour.city,
+  //   country: tour.country,
+  // })
   return (
     <div>
       <Link href='/tours' className='btn btn-secondary mb-12'>
