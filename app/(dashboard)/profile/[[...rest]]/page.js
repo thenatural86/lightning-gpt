694f5af1875961ef633dfe3e8fba132568503473
thenatural86 <philip.smalls86@gmail.com>
2025-01-24 14:12:06 -0500

    displays user tokens

--- app/(dashboard)/profile/[[...rest]]/page.js
+++ app/(dashboard)/profile/[[...rest]]/page.js
@@ -1,8 +1,15 @@
+import { fetchUserTokensById } from '@/utils/action'
 import { UserProfile } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+  const { userId } = auth()
+  const currentTokens = await fetchUserTokensById(userId)
   return (
     <div>
+      <h2 className='mb-8 ml-8 text-xl font-extrabold'>
+        Token Amount: {currentTokens}
+      </h2>
       <UserProfile />
     </div>
   )
