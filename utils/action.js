240493f22ffadc2fda387df66e59c6ef79964a66
thenatural86 <philip.smalls86@gmail.com>
2025-01-24 23:18:24 -0500

    shorten gpt response

--- utils/action.js
+++ utils/action.js
@@ -39,7 +39,7 @@ Once you have a list, create a one-day tour. Response should be in the following
     "country": "${country}",
     "title": "title of the tour",
     "description": "description of the city and tour",
-    "stops": ["short paragraph on the stop 1 ", "short paragraph on the stop 2","short paragraph on the stop 3"]
+    "stops": ["stop name ", "stop name","stop name"]
   }
 }
 If you can't find info on exact ${city}, or ${city} does not exist, or it's population is less than 1, or it is not located in the following ${country} return { "tour": null }, with no additional characters.`
